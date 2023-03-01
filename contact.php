4203590561f596122d5eeb7daa07561e2d84f1ff
Sandhyavikram11 <126644106+Sandhyavikram11@users.noreply.github.com>
2023-03-01 11:51:54 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,131 @@
+<?php
+include("header.php");
+?>
+
+<div id="page-header">
+
+<div class="section-bg" style="background-image: url(img/background-2.jpg);"></div>
+
+
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="header-content">
+<h1>Contact us</h1>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+</header>
+
+
+<div class="section">
+
+<div class="container">
+
+<div class="row">
+
+<main id="main" class="col-md-7">
+
+<div class="article causes-details">
+
+
+
+<div class="article-tags-share">
+
+
+<ul class="share">
+<li>SHARE:</li>
+<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+</ul>
+
+</div>
+
+
+<div class="article-reply">
+<h3>Contact us.</h3>
+<p>Leave a message...</p>
+<form>
+<div class="row">
+<div class="col-md-6">
+<div class="form-group">
+<input class="input" placeholder="Name" type="text">
+</div>
+</div>
+<div class="col-md-6">
+<div class="form-group">
+<input class="input" placeholder="Email" type="email">
+</div>
+</div>
+<div class="col-md-12">
+<div class="form-group">
+<input class="input" placeholder="Subject" type="text">
+</div>
+</div>
+<div class="col-md-12">
+<div class="form-group">
+<textarea class="input" placeholder="Message"></textarea>
+</div>
+<button class="primary-button">Submit</button>
+</div>
+</div>
+</form>
+</div>
+
+</div>
+
+</main>
+
+
+<aside id="aside" class="col-md-5">
+
+<div class="widget">
+<h3 class="widget-title">Address</h3>
+<div class="widget-category">
+ Sahaya Charity,
+    Opp. konaje police station,
+    Bangalore-574199
+</div>
+</div>
+
+
+<div class="widget">
+<h3 class="widget-title">Contact No.</h3>
+<div class="widget-category">
+0824-2287236
+</div>
+</div>
+
+<div class="widget">
+<h3 class="widget-title">Email ID.</h3>
+<div class="widget-category">
+abhayaAshraya@gmail.com
+</div>
+</div>
+
+</aside>
+
+</div>
+
+</div>
+
+
+</div>
+
+<!-- //contact -->
+	<!-- map --> 
+	<div class="map p-2">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.905564518001!2d77.61314891434468!3d12.913790790893785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae14fb1629ad85%3A0x599b9a8d5363f526!2sVidya%20Jyothi%20Primary%20and%20High%20School!5e0!3m2!1sen!2sin!4v1660716654740!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	
+	</div>
+	<!-- //map -->
+<?php
+include("footer.php");
+?>
---
+++ databaseconnection.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+// Create connection
+$con=mysqli_connect("localhost","root","","OCMS");
+echo mysqli_connect_error();
+?>
